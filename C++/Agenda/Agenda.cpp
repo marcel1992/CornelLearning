1ecfe3d9a868ab64d372ddd3126e4c0cb34608a1
marcel1992 <www.valachmarcel@gmail.com>
2019-07-04 22:02:37 +0300

    Update Agenda.cpp

--- C++/Agenda/Agenda.cpp
+++ C++/Agenda/Agenda.cpp
@@ -1,21 +1,64 @@
-// Agenda.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "pch.h"
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	class Agenda
+	{
+	public:
+		/*
+			string Name;  // nume
+			string Surname;//prenume
+			int BirthDate; //data nastere
+			string Gender;//gen
+			string Address; //adresa
+		 */
+
+		string Nume;
+		double Ani;
+		string gen;
+
+		void type()
+		{
+			cout << "Introdu numele tau: ";
+			cin >> Nume;
+
+			cout << "\nIntrodu ani tai: ";
+			cin >> Ani;
+
+			cout << "\nMasculin/Feminin/Neutru\nCare este genul tau: ";
+			cin >> gen;
+
+			cout << "\nDatele tale sunt: ";
+			cout << "\n";
+			cout << "\n";
+			cout << "Numele: ";
+		cout << Nume;
+		cout << "\n";
+		cout << "Ani: ";
+		cout << Ani;
+		cout << "\n";
+		cout << "Genul: ";
+		cout << gen;
+
+		cout << "\n";
+		cout << "\n";
+		cout << "\n";
+		cout << "\n";
+		}
+	};
+		
+	
+	Agenda nume;
+
+	nume.type();
+
+	cin.get();
 	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
